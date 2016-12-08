ef31675b0742d9eb6fc52aeef17209342fa248a4
JimTheMan <mrdotjim@gmail.com>
2016-12-08 17:03:38 -0500

    added units tests example for ngrx reducer

--- jims-ngrx-eg/src/app/state-management/reducers/main-reducer.spec.ts
+++ jims-ngrx-eg/src/app/state-management/reducers/main-reducer.spec.ts
@@ -2,40 +2,43 @@
  * Created by jlynch on 12/2/16.
  */
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import {async, TestBed, inject} from '@angular/core/testing';
 import {mainReducer} from "./main-reducer";
-import { StoreModule } from "@ngrx/store";
+import {StoreModule} from "@ngrx/store";
 import {State} from "../state/main-state";
-import { Store } from "@ngrx/store";
+import {Store} from "@ngrx/store";
 
 describe('Reducer', () => {
-  // let component: SmartComponentComponent;
-  // let fixture: ComponentFixture<SmartComponentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-        declarations: [  ],
+        declarations: [],
         imports: [StoreModule.provideStore({mainReducer})]
       })
       .compileComponents();
   }));
 
-  beforeEach(() => {
-    // fixture = TestBed.createComponent(SmartComponentComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
-  });
+  it('should have the intial counter be 10', inject([Store], (store:Store<State>) => {
+    expect(store).toBeTruthy();
 
-  // it('should create', inject([Store], (store: Store<State>) => {
-  //   expect(store).toBeTruthy();
-  //
-  //
-  //   store.dispatch({ type: "INCREMENT", payload: {innerObj: {text: "derp!"}} });
-  //
-  //
-  //
-  // }));
+    let data = store.select('mainReducer')
+      .subscribe((data:State)=> {
+        expect(data.counter).toEqual(10);
+
+      });
+
+  }));
+
+  it('should have the counter be 11 after handling the INCREMENT action', inject([Store], (store:Store<State>) => {
+    expect(store).toBeTruthy();
+
+    store.dispatch({ type: "INCREMENT", payload: {innerObj: {text: "derp!"}} });
+
+    let data = store.select('mainReducer')
+      .subscribe((data:State)=> {
+        expect(data.counter).toEqual(11);
+
+      });
+
+  }));
 });
